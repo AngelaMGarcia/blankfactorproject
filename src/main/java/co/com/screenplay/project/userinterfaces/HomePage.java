b585d7b732a0f1287693e34e494b0ca87bf75590
Alex Andrade <alexander.andrade@gmail.com>
2024-10-02 19:05:03 -0500

    🔧 update locators

--- src/main/java/co/com/screenplay/project/userinterfaces/HomePage.java
+++ src/main/java/co/com/screenplay/project/userinterfaces/HomePage.java
@@ -8,12 +8,10 @@ import static net.serenitybdd.screenplay.targets.Target.the;
 
 public class HomePage extends PageObject {
 
-    public static final Target BTN_SEARCH= the("btn buscar").located(By.className("search-submit"));
+    public static final Target BTN_SEARCH = the("btn buscar").located(By.id("search-toggle"));
     public static final Target SEARCH = the("label de busqueda").located(By.id("search-form-2"));
-    public static final Target ARTICLE = the("articulo").located(By.className(" entry-title"));
-    public static final Target VALIDATE_ARTICLE = the("articulo").located(By.className(" newspack-post-subtitle"));
-
-
+    public static final Target ARTICLE = the("articulo").located(By.className("entry-title"));
+    public static final Target VALIDATE_ARTICLE = the("articulo").located(By.className("newspack-post-subtitle"));
 
 
 }
