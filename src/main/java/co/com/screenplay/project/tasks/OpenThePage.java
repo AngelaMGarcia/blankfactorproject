9a00c983c914dc5fdd37b472976be22bb3c9cacd
Alex Andrade <alexander.andrade@gmail.com>
2024-10-02 19:05:39 -0500

    🔧 update actions

--- src/main/java/co/com/screenplay/project/tasks/OpenThePage.java
+++ src/main/java/co/com/screenplay/project/tasks/OpenThePage.java
@@ -7,6 +7,7 @@ import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.actions.Enter;
 import net.serenitybdd.screenplay.actions.Scroll;
+import org.openqa.selenium.Keys;
 
 import static co.com.screenplay.project.userinterfaces.HomePage.*;
 import static net.serenitybdd.screenplay.Tasks.instrumented;
@@ -17,15 +18,15 @@ public class OpenThePage implements Task {
     @Override
     public <T extends Actor> void performAs(T actor) {
         actor.attemptsTo(Click.on(BTN_SEARCH),
-                Click.on(SEARCH),
-                Enter.theValue(Data.Name).into(SEARCH),
+                Enter.theValue(Data.Name).into(SEARCH).thenHit(Keys.ENTER),
                 Scroll.to(ARTICLE),
                 Click.on(ARTICLE)
-                );
+        );
 
 
     }
-    public static OpenThePage hipertextual(){
+
+    public static OpenThePage hipertextual() {
         return instrumented(OpenThePage.class);
 
     }
